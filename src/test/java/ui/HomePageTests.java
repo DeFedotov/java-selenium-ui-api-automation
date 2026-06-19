5b15748e9c90d5041d457d28e403f02550d89dc0
dzianis <fd.postbox@gmail.com>
2026-06-19 13:08:31 +0200

    ui: add ui tests to check that all chapter links from the home page can be opened

--- src/test/java/ui/HomePageTests.java
+++ src/test/java/ui/HomePageTests.java
@@ -5,13 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HomePageTests {
-    WebDriver driver;
+    private WebDriver driver;
     private static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
 
     @BeforeEach
@@ -23,26 +22,159 @@ class HomePageTests {
 
     @AfterEach
     void tearDown() {
-        driver.quit();
-    }
-
-    @Test
-    void openHomePageTest() {
-        String actualTitle = driver.getTitle();
-
-        assertEquals("Hands-On Selenium WebDriver with Java",  actualTitle);
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
     void openWebFormPageTest() {
-        String expectedUrl = BASE_URL + "web-form.html";
-        String expectedPageName = "Web form";
+        verifyPage("web-form.html", "Web form", "Chapter 3. WebDriver Fundamentals");
+    }
 
-        driver.findElement(By.xpath("//a[@href='web-form.html']")).click();
+    @Test
+    void openNavigationPageTest() {
+        verifyPage("navigation1.html", "Navigation example", "Chapter 3. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openDropDownPageTest() {
+        verifyPage("dropdown-menu.html", "Dropdown menu",  "Chapter 3. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openMouseOverPageTest() {
+        verifyPage("mouse-over.html", "Mouse over",  "Chapter 3. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openDrugAndDropPageTest() {
+        verifyPage("drag-and-drop.html",  "Drag and drop",   "Chapter 3. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openDrawInCanvasPageTest() {
+        verifyPage("draw-in-canvas.html", "Drawing in canvas",  "Chapter 3. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openLoadingImagesPageTest() {
+        verifyPage("loading-images.html", "Loading images",   "Chapter 3. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openSlowCalculatorPageTest() {
+        verifyPage("slow-calculator.html", "Slow calculator",    "Chapter 3. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openLongPageTest() {
+        verifyPage("long-page.html", "This is a long page",   "Chapter 4. Browser-Agnostic Features");
+    }
+
+    @Test
+    void openInfiniteScrollPageTest() {
+        verifyPage("infinite-scroll.html", "Infinite scroll",   "Chapter 4. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openShadowDomPageTest() {
+        verifyPage("shadow-dom.html", "Shadow DOM",   "Chapter 4. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openCookiesPageTest() {
+        verifyPage("cookies.html", "Cookies",   "Chapter 4. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openFramesPageTest() {
+        verifyPage("frames.html", "Frames",   "Chapter 4. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openIframesPageTest() {
+        verifyPage("iframes.html",  "IFrame",   "Chapter 4. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openDialogBoxesPageTest() {
+        verifyPage("dialog-boxes.html",  "Dialog boxes",   "Chapter 4. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openWebStoragePageTest() {
+        verifyPage("web-storage.html",  "Web storage",   "Chapter 4. WebDriver Fundamentals");
+    }
+
+    @Test
+    void openGeolocationPageTest() {
+        verifyPage("geolocation.html",  "Geolocation",   "Chapter 5. Browser-Specific Manipulation");
+    }
+
+    @Test
+    void openNotificationsPageTest() {
+        verifyPage("notifications.html",  "Notifications",   "Chapter 5. Browser-Specific Manipulation");
+    }
+
+    @Test
+    void openGetUserMediaPageTest() {
+        verifyPage("get-user-media.html", "Get user media",  "Chapter 5. Browser-Specific Manipulation");
+    }
+
+    @Test
+    void openMultilanguagePageTest() {
+        verifyPage("multilanguage.html", "Multilanguage page",  "Chapter 5. Browser-Specific Manipulation");
+    }
+
+    @Test
+    void openConsoleLogsPageTest() {
+        verifyPage("console-logs.html", "Console logs",   "Chapter 5. Browser-Specific Manipulation");
+    }
+
+    @Test
+    void openLoginFormPageTest() {
+        verifyPage("login-form.html", "Login form", "Chapter 7. The Page Object Model (POM)");
+    }
+
+    @Test
+    void openSlowLoginPageTest() {
+        verifyPage("login-slow.html", "Slow login form", "Chapter 7. The Page Object Model (POM)");
+    }
+
+    @Test
+    void openRandomCalculatorPageTest() {
+        verifyPage("random-calculator.html",  "Random calculator",   "Chapter 8. Testing Framework Specifics");
+    }
+
+    @Test
+    void openDownloadFilesPageTest() {
+        verifyPage("download.html", "Download files",   "Chapter 9. Third-Party Integrations");
+    }
+
+    @Test
+    void openAbTestingPageTest() {
+        verifyPage("ab-testing.html", "A/B Testing",    "Chapter 9. Third-Party Integrations");
+    }
+
+    @Test
+    void openDataTypesPageTest() {
+        verifyPage("data-types.html", "Data types",   "Chapter 9. Third-Party Integrations");
+    }
+
+    void verifyPage(String pageHtml, String expectedPageName, String chapterName) {
+        String expectedUrl = BASE_URL + pageHtml;
+
+        String complexXpath = "//h5[text()='" + chapterName + "']/../a[contains(@href, '" + pageHtml + "')]";
+
+        driver.findElement(By.xpath("//a[@href='" + pageHtml + "']")).click();
+
         String actualUrl = driver.getCurrentUrl();
         String actualPageName = driver.findElement(By.className("display-6")).getText();
 
         assertEquals(expectedUrl, actualUrl, "Web form url is incorrect.");
         assertEquals(expectedPageName, actualPageName, "Page name is incorrect.");
+
+        driver.navigate().back();
     }
 }
