7b6e6091df83b04dbbc50e3f4e362b0da793713d
dzianis <fd.postbox@gmail.com>
2026-07-08 19:56:59 +0200

    Feat: Add test for cookies page - read cookies

---
+++ src/test/java/ui/CookiesPageTests.java
@@ -0,0 +1,41 @@
+package ui;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+
+public class CookiesPageTests {
+    WebDriver driver;
+    private static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
+    private static final String COOKIES_URL = BASE_URL + "cookies.html";
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get(COOKIES_URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Read cookies test")
+    public void readCookiesTest() {
+        WebDriver.Options options = driver.manage();
+        Set<Cookie> cookies = options.getCookies();
+        Assertions.assertEquals(cookies.size(), 2);
+
+        Cookie username = options.getCookieNamed("username");
+        Assertions.assertEquals(username.getValue(), "John Doe");
+        Assertions.assertEquals(username.getPath(), "/");
+
+        driver.findElement(By.id("refresh-cookies")).click();
+    }
+}
