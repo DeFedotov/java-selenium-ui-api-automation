1c1a336eef3516c426291b9efb7b803658b9cf91
Dzianis Fiadotau <45372503+DeFedotov@users.noreply.github.com>
2026-07-28 20:41:37 +0200

    Feature/dialog boxes tests (#21)
    * Feat: Add alert test for dialog boxes page
    
    * Feat: Add confirm test for dialog boxes page
    
    * Feat: Add prompt test for dialog boxes page
    
    * Feat: Add modal window test for dialog boxes page

---
+++ src/test/java/ui/DialogBoxesPageTests.java
@@ -0,0 +1,75 @@
+package ui;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DialogBoxesPageTests {
+    WebDriver driver;
+    private static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
+    private static final String DIALOG_BOXES_URL = BASE_URL + "dialog-boxes.html";
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get(DIALOG_BOXES_URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Alert test")
+    public void alertTest() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.findElement(By.id("my-alert")).click();
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals(alert.getText(), "Hello world!");
+        alert.accept();
+    }
+
+    @Test
+    @DisplayName("Confirm test")
+    public void confirmTest() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.findElement(By.id("my-confirm")).click();
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert confirm = driver.switchTo().alert();
+        Assertions.assertEquals(confirm.getText(), "Is this correct?");
+        confirm.dismiss();
+    }
+
+    @Test
+    @DisplayName("Prompt name")
+    public void promptTest() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.findElement(By.id("my-prompt")).click();
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert prompt = driver.switchTo().alert();
+        prompt.sendKeys("John Doe");
+        Assertions.assertEquals(prompt.getText(), "Please enter your name");
+        prompt.accept();
+    }
+
+    @Test
+    @DisplayName("Modal window test")
+    public void modalWindowTest() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.findElement(By.id("my-modal")).click();
+        WebElement close =  driver.findElement(By.xpath("//button[text()='Close']"));
+        Assertions.assertEquals(close.getTagName(), "button");
+        wait.until(ExpectedConditions.elementToBeClickable(close));
+        close.click();
+    }
+}
