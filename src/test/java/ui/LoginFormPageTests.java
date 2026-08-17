28b26b96e8b3a85d0b20a6fa767163721b1abd9e
dzianis <fd.postbox@gmail.com>
2026-08-17 20:02:28 +0200

    Feat: Add owner library

--- src/test/java/ui/LoginFormPageTests.java
+++ src/test/java/ui/LoginFormPageTests.java
@@ -1,21 +1,22 @@
 package ui;
 
+import configs.TestPropertiesConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static constants.Constants.BASE_URL;
-
 public class LoginFormPageTests {
     WebDriver driver;
-    private static final String LOGIN_FORM_URL = BASE_URL + "login-form.html";
+    TestPropertiesConfig config = ConfigFactory.create(TestPropertiesConfig.class, System.getProperties());
+    private static final String LOGIN_FORM_URL = "login-form.html";
 
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
-        driver.get(LOGIN_FORM_URL);
+        driver.get(config.getBaseUrl() + LOGIN_FORM_URL);
         driver.manage().window().maximize();
     }
 
@@ -27,8 +28,8 @@ public class LoginFormPageTests {
     @Test
     @DisplayName("Login form test")
     public void loginFormTest() {
-        driver.findElement(By.id("username")).sendKeys("user");
-        driver.findElement(By.id("password")).sendKeys("user");
+        driver.findElement(By.id("username")).sendKeys(config.getUsername());
+        driver.findElement(By.id("password")).sendKeys(config.getPassword());
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         WebElement message = driver.findElement(By.id("success"));
 
