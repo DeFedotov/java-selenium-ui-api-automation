8c124bee848aca9dcd924f76bf963b6cd230cf37
dzianis <fd.postbox@gmail.com>
2026-08-26 19:40:29 +0200

    Feat: Add home page to pom

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,20 +1,30 @@
 package pageObjects;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 import static constants.Constants.BASE_URL;
 
 public class HomePage extends BasePage{
+    public static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
+
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
+    @Step("Open Home Page")
     public void openHomePage() {
         driver.get(BASE_URL);
     }
 
+    @Step("Get Home Page URL")
     public String getHomePageUrl() {
         return BASE_URL;
+    }
+
+    @Step("Get Home Page Title")
+    public String getHomePageTitle() {
+        return driver.getTitle();
     }
 }
