67dfed5693c67be1491098bf974805d9e8d9c2c1
dzianis <fd.postbox@gmail.com>
2026-08-27 12:24:40 +0200

    Feat: Add home page to pom part 2

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,13 +1,19 @@
 package pageObjects;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import static constants.Constants.BASE_URL;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HomePage extends BasePage{
     public static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
 
+    By chapterLocator = By.cssSelector("h5.card-title");
+    By linkLocator = By.xpath("./../a");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -26,5 +32,32 @@ public class HomePage extends BasePage{
     @Step("Get Home Page Title")
     public String getHomePageTitle() {
         return driver.getTitle();
+    }
+
+    @Step("Count all chapters on Home Page")
+    public int countChapters(){
+        int qtyChapters = 0;
+        List<WebElement> chapters = driver.findElements(chapterLocator);
+        for (WebElement chapter : chapters) {
+            qtyChapters += 1;
+        }
+        return qtyChapters;
+    }
+
+    @Step("Count and click all Home Page chapters links")
+    public int countAndClickLinks() {
+        int qtyLinks = 0;
+        List<WebElement> chapters = driver.findElements(chapterLocator);
+        for (WebElement chapter : chapters) {
+            List<WebElement> links = chapter.findElements(linkLocator);
+            qtyLinks += links.size();
+            System.out.println(chapter.getText());
+            for (WebElement link : links) {
+                System.out.println(link.getText());
+                link.click();
+                driver.navigate().back();
+            }
+        }
+        return qtyLinks;
     }
 }
