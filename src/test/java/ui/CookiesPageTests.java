d06ef94f1bc0f5cdf140152b415bb4c61a75b683
dzianis <fd.postbox@gmail.com>
2026-09-21 12:38:02 +0200

    Feat: Add cookies page to pom 3

--- src/test/java/ui/CookiesPageTests.java
+++ src/test/java/ui/CookiesPageTests.java
@@ -1,75 +1,14 @@
 package ui;
 
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
-import org.openqa.selenium.WebDriver;
 import pageObjects.CookiesPage;
 import pageObjects.HomePage;
 
-import java.util.Set;
-
-import static constants.Constants.COOKIES_URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CookiesPageTests extends BaseTest{
 
-    @BeforeEach
-    public void setup() {
-        driver.get(COOKIES_URL);
-    }
-
-    @Test
-    @DisplayName("Read cookies test")
-    public void readCookiesTest() {
-        WebDriver.Options options = driver.manage();
-        Set<Cookie> cookies = options.getCookies();
-        assertEquals(cookies.size(), 2);
-
-        Cookie username = options.getCookieNamed("username");
-        assertEquals(username.getValue(), "John Doe");
-        assertEquals(username.getPath(), "/");
-
-        driver.findElement(By.id("refresh-cookies")).click();
-    }
-
-    @Test
-    @DisplayName("Add cookies test")
-    public void addCookiesTest() {
-        WebDriver.Options options = driver.manage();
-        Cookie newCookie = new Cookie("new-cookie-key", "new-cookie-value");
-        options.addCookie(newCookie);
-
-        String readValue = options.getCookieNamed(newCookie.getName()).getValue();
-        assertEquals(newCookie.getValue(), readValue);
-
-        driver.findElement(By.id("refresh-cookies")).click();
-    }
-
-    @Test
-    @DisplayName("Edit cookie test")
-    public void editCookieTest(){
-        WebDriver.Options options = driver.manage();
-        Cookie username = options.getCookieNamed("username");
-        Cookie editedCookie = new Cookie(username.getName(), "new-value");
-        options.addCookie(editedCookie);
-
-        Cookie readCookie  = options.getCookieNamed(username.getName());
-        assertEquals(editedCookie, readCookie);
-
-        driver.findElement(By.id("refresh-cookies")).click();
-    }
-
-    @Test
-    @DisplayName("Delete cookie test")
-    public void deleteCookieTest(){
-        WebDriver.Options options = driver.manage();
-        Set<Cookie> cookies = options.getCookies();
-        Cookie username = options.getCookieNamed("username");
-        options.deleteCookie(username);
-
-        assertEquals(cookies.size()-1, options.getCookies().size());
-    }
 
     @Test
     @DisplayName("Get cookies test")
@@ -109,5 +48,35 @@ public class CookiesPageTests extends BaseTest{
 
         String actualNewCookieValue = cookiesPage.getCookieByName(newCookie.getName());
         assertEquals(newCookie.getValue(), actualNewCookieValue);
+    }
+
+    @Test
+    @DisplayName("Edit cookie test")
+    public void editCookieByNameTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.openHomePage();
+        homePage.openCookiesPage();
+
+        CookiesPage cookiesPage = new CookiesPage(driver);
+        cookiesPage.editCookie("username", "new-username-value");
+
+        String actualNewCookieValue = cookiesPage.getCookieByName("username");
+        assertEquals("new-username-value", actualNewCookieValue);
+
+        cookiesPage.refreshCookies();
+    }
+
+    @Test
+    @DisplayName("Delete cookie test")
+    public void deleteCookieByNameTest() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        homePage.openHomePage();
+        homePage.openCookiesPage();
+
+        CookiesPage cookiesPage = new CookiesPage(driver);
+        cookiesPage.deleteCookie("username");
+        Thread.sleep(3000);
+
+        assertEquals(1, cookiesPage.getCookiesSize());
     }
 }
